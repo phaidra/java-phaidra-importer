17769b9663aebd6de525b216553b475b42117a66
Mauro Medelin <m.medelin@im-tech.it>
2014-07-22 14:58:47 +0200

    Moved subproject updater

---
+++ Updater/bookimporter/eodeditor/ComboVocabulary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace eodeditor
+{
+    class ComboVocabulary
+    {
+        string ID = null;
+        string description = null;
+        public ComboVocabulary(string ID, string description)
+        {
+            this.ID = ID;
+            this.description = description;
+        }
+        public override string ToString()
+        {
+            return this.description;
+        }
+        public string getValue()
+        {
+            return this.ID;
+        }
+    }
+}
